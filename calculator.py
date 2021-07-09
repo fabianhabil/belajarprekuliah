1964052697447d709f0d51655db4ffd22ea70010
daverussell13 <76587424+daverussell13@users.noreply.github.com>
2021-07-09 21:42:49 +0700

    Force the user to input integer
    making indefinite loop if user insert alphabetical input

--- calculator.py
+++ calculator.py
@@ -1,22 +1,25 @@
 print("Calculator bianha v.0.1")
 
 num1 = input("Angka berapa? ")
-if (num1.isalpha()):
+while (num1.isalpha()):
     print("salah!")
     num1 = input("Angka berapa? ")
+
 num2 = input("Angka berapa? ")
+while (num2.isalpha()):
+    print("salah!")
+    num2 = input("Angka berapa? ")
+
 operation = input("Operasi hitung? (pakai simbol) ")
 
-if (num2.isalpha()):
-    print("salah!")
-elif operation=="+":
+if operation=="+":
     print(float(num1) + float(num2))
 elif operation=="-":
     print(float(num1) - float(num2))
 elif operation=="/":
     print(float(num1) / float(num2))
 elif operation=="*":
-    print(float(num1) * float(num2)) 
-
+    print(float(num1) * float(num2))
+    
 # print(type(num1))
 # print(type(num2))
